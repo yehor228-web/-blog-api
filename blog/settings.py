8530740f74c9c9fd666d8e6bab91b48a30e05000
Yehor Maksymenko <yehor.maksymenko@gmail.com>
2025-10-30 20:59:49 +0200

    change setings

--- blog/settings.py
+++ blog/settings.py
@@ -1,18 +1,21 @@
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zc)cb1shwml9$9mk&d2-6o_55-a*z-@g6n=nqvw(=ptue9ca#_"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
+
+
+
 
 ALLOWED_HOSTS = []
 
@@ -24,7 +27,7 @@ EMAIL_HOST_USER = 'yehor.maksymenko@gmail.com'
 EMAIL_HOST_PASSWORD = 'ryiu ouuf ttuz zzrf'  # App Password з Google
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-# Application definition
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -151,8 +154,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='yehor.maksymenko@gmail.com'
-EMAIL_HOST_PASSWORD='duuo ptga kcjh kuac'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
 
